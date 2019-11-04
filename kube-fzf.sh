f7ef2a62a4272752e251815d158b166fc1c25fd7
johan <johan@thoughtmachine.net>
2019-11-04 17:18:49 +0000

    get findpod working with context and remove namespace fuzzy searcher

--- kube-fzf.sh
+++ kube-fzf.sh
@@ -65,26 +65,24 @@ EOF
 }
 
 _kube_fzf_handler() {
-  local opt namespace_query pod_query cmd
+  local opt pod_query cmd
   local open=false
   local copy=false
   local context="minikube"
+  local namespace=""
   local OPTIND=1
   local func=$1
 
   shift $((OPTIND))
 
-  while getopts ":hn:aocC:" opt; do
+  while getopts ":hn:ocC:" opt; do
     case $opt in
       h)
         _kube_fzf_usage "$func"
         return 1
         ;;
       n)
-        namespace_query="$OPTARG"
-        ;;
-      a)
-        namespace_query="--all-namespaces"
+        namespace="$OPTARG"
         ;;
       o)
         open=true
@@ -136,7 +134,7 @@ _kube_fzf_handler() {
     pod_query=$1
   fi
 
-  args="$context|$namespace_query|$pod_query|$cmd|$open|$copy"
+  args="$context|$namespace|$pod_query|$cmd|$open|$copy"
 }
 
 _kube_fzf_fzf_args() {
@@ -150,33 +148,15 @@ _kube_fzf_fzf_args() {
 _kube_fzf_search_pod() {
   local namespace pod_name
   local context=$1
-  local namespace_query=$2
+  local namespace=$2
   local pod_query=$3
   local pod_fzf_args=$(_kube_fzf_fzf_args "$pod_query")
 
-  if [ -z "$namespace_query" ]; then
-      namespace=$(kubectl config get-contexts --no-headers $context \
-        | awk '{ print $6 }')
+  [ -z $namespace ] && [ $context -eq "minikube" ] namespace='default' || namespace=$context
 
-      namespace=${namespace:=default}
-      pod_name=$(kubectl get pod --context=$context --namespace=$namespace --no-headers \
-          | fzf $(echo $pod_fzf_args) \
-        | awk '{ print $2 }')
-  elif [ "$namespace_query" = "--all-namespaces" ]; then
-    read namespace pod_name <<< $(kubectl get pod --context=$context --all-namespaces --no-headers \
-        | fzf $(echo $pod_fzf_args) \
-      | awk '{ print $2, $3 }')
-  else
-    local namespace_fzf_args=$(_kube_fzf_fzf_args "$namespace_query" "--select-1")
-    namespace=$(kubectl get namespaces --context=$context --no-headers \
-        | fzf $(echo $namespace_fzf_args) \
-      | awk '{ print $2 }')
-
-    namespace=${namespace:=default}
-    pod_name=$(kubectl get pod --context=$context --namespace=$namespace --no-headers \
-        | fzf $(echo $pod_fzf_args) \
-      | awk '{ print $2 }')
-  fi
+  pod_name=$(kubectl get pod --context=$context --namespace=$namespace --no-headers \
+                 | fzf $(echo $pod_fzf_args) \
+                 | awk '{ print $2 }')
 
   [ -z "$pod_name" ] && return 1
 
